997bacdd6ba07e4544220c5341c8ac26d5ce7ea8
AziaRae <jmespino2@up.edu.ph>
2026-05-08 10:51:54 +0800

    rawr

--- a.c
+++ a.c
@@ -1,5 +1,21 @@
 #include "operations_research_i.h"
 
-int64_t num_points_of_interest(int x0, int y0, int r) {
-    
+int64_t newtonRaphsonSqrt(int64_t n) {
+
 }
+
+int64_t num_points_of_interest(int x0, int y0, int r) {
+    int64_t up = y0 + r, down = y0 - r;
+    int64_t left, right;
+    int64_t verticalDistance;
+    int64_t horizontalHalfLength;
+
+    for (int i = up; i > y0; i--) {
+        verticalDistance = i - y0;
+        horizontalHalfLength = newtonRaphsonSqrt(r * r - verticalDistance * verticalDistance);
+
+    }
+}
+
+// f(x) = x^(1/2)
+// f'(x) = 
---
+++ d_main.c
@@ -0,0 +1,11 @@
+#include "gollums_columns.h"
+
+void test1() {
+    char **res = transpose(3, 6, {"abcdef", "gehijk", "lmnopq"});
+}
+
+int main() {
+
+
+    return 0;
+}
